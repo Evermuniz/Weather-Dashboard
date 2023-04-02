a55d6aeba91585d3c6b778c5e32b6174820da292
evermuniz <evermuniz@outlook.com>
2023-04-02 17:58:26 -0600

    added 5 day forecast with date

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var button = document.querySelector("#submission");
 var main = document.querySelector("#main");
 var title = document.querySelector("#current");
 var icon = document.querySelector("#icon");
+var fiveDayContainer = document.querySelector("#fiveDay");
+var date = dayjs().format('M/D/YYYY');
 
 function coordinates(search) {
   var coord = apiURL + "/geo/1.0/direct?q=" + search + "&limit=5&appid=" + apiKey;
@@ -40,6 +42,7 @@ function getWeather() {
 
 function items(city, data) {
   displayWeather(city, data.list[0]);
+  fiveDayForecast(data.list);
 }
 
 function displayWeather(city, data) {
@@ -48,7 +51,7 @@ function displayWeather(city, data) {
   var temp = data.main.temp;
   var wind = data.wind.speed;
   var humidity = data.main.humidity;
-  var todaysDate = dayjs(data.weather[0].dt_text).format('M/D/YYYY');
+  var todaysDate = dayjs(data.weather[0].dt_text).format("M/D/YYYY");
 
   var container = document.createElement("div");
   var tempData = document.createElement("p");
@@ -56,27 +59,42 @@ function displayWeather(city, data) {
   var humidityData = document.createElement("p");
 
   icon.setAttribute("src", image);
-  icon.setAttribute('class', 'image');
-  icon.setAttribute('alt', description);
-  title.textContent = city +' ' + todaysDate;
+  icon.setAttribute("class", "image");
+  icon.setAttribute("alt", description);
+  title.textContent = city + " " + todaysDate;
   tempData.textContent = "Temperature: " + temp + "°F";
   windData.textContent = "Wind Speed: " + wind + " MPH";
   humidityData.textContent = "Humidity " + humidity + " %";
 
   main.appendChild(container);
   container.append(tempData, windData, humidityData);
-};
-
-function fiveDayForecast () {
-
-
-
 }
 
+function fiveDayForecast(data) {
+  for (var i = 1; i < 6; i++) {
 
+   // var image = "https://openweathermap.org/img/wn/" + data.weather[i].icon + ".png";
+    //   var description = data[i].weather[0].description;
+    var temp = data[i].main.temp;
+    var wind = data[i].wind.speed;
+    var humidity = data[i].main.humidity;
+    var future = dayjs().add(i, 'day').format('M/D/YYYY');
 
+    var card = document.createElement("div");
+    var dateText = document.createElement("h4");
+    var tempData = document.createElement("p");
+    var windData = document.createElement("p");
+    var humidityData = document.createElement("p");
 
+    dateText.textContent = future;
 
+    tempData.textContent = "Temperature: " + temp + "°F";
+    windData.textContent = "Wind Speed: " + wind + " MPH";
+    humidityData.textContent = "Humidity " + humidity + " %";
 
+    fiveDayContainer.appendChild(card);
+    card.append(dateText, tempData, windData, humidityData);
+  }
+}
 
 button.addEventListener("click", getWeather);
